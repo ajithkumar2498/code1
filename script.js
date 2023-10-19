95e8206879942a73a01a1b2b577881735d40c8fb
ajithkumar2498 <ajibhc24@gmail.com>
2023-10-19 06:47:07 +0530

    hii

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("hii");
+let div1 = document.createElement("div");
+div1.setAttribute("id","container")
 
+let div2 = document.createElement("div");
+div2.setAttribute("id","row")
+
+let div3 = document.createElement("div");
+div3.setAttribute("id","column")
+
+
+
+div2.appendChild(div3)
+div1.appendChild(div2)
+document.body.append(div1);
+document.body.append(div2);
+document.body.append(div3);
